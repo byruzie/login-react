59c8bcfc24efea4d78f78cb889834b269803f0bc
Raul Cardoso <byruzie@gmail.com>
2025-03-24 12:12:09 -0300

    first commit

---
+++ my-project/src/pages/SignIn.jsx
@@ -0,0 +1,7 @@
+function SignIn() {
+    return(
+        <div>Hello World</div>
+    )
+}
+
+export default SignIn
---
+++ my-project/src/pages/SignUp.jsx
@@ -0,0 +1,7 @@
+function SignUp() {
+    return(
+        <div>Hello World</div>
+    )
+}
+
+export default SignUp
