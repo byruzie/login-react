c0c472fd7b03c90b310f7718a9a0a3e25214a4ce
Raul Cardoso <byruzie@gmail.com>
2025-03-25 14:01:25 -0300

    first version sign up page

--- my-project/src/components/SignUpForm.jsx
+++ my-project/src/components/SignUpForm.jsx
@@ -1,7 +1,39 @@
 function SignUpForm() {
     return (
-        <div>
-            Olá
+        <div className="h-screen w-screen flex justify-center items-center">
+            <div className="w-md flex flex-col 2xl:w-xl">
+                <div className="flex">
+                    <div className="w-full flex flex-col">
+                        <label htmlFor="">First Name</label>
+                        <input className="border rounded-md pl-2 py-1" type="text" placeholder="First Name" />
+                    </div>
+                    <div className="w-full ml-2 flex flex-col">
+                        <label>Last Name</label>
+                        <input className="border rounded-md pl-2 py-1" type="text" placeholder="Last Name" />
+                    </div>
+                </div>
+                <div className="mt-2">
+                    <div className="flex flex-col">
+                        <label>Email</label>
+                        <input className="border rounded-md pl-2 py-1" type="text" placeholder="Email" />
+                    </div>
+                    <div className="flex flex-col mt-2">
+                        <label htmlFor="">Password</label>
+                        <input className="border rounded-md pl-2 py-1" type="text" placeholder="Password" />
+                    </div>
+                    <div className="mt-6">
+                        <button className="w-full bg-black text-white rounded-md p-1 font-semibold cursor-pointer">Create account</button>
+                    </div>
+                </div>
+                <div className="mt-12">
+                    <div className="w-full">
+                        <h4 className="text-center text-gray-400">or continue with</h4>
+                        <button className="w-full border rounded-md p-1 font-semibold cursor-pointer mt-2">Login with Google</button>
+                        <p className="mt-2 text-center text-gray-400">By continuing you agree to <a className="font-semibold text-gray-900" href="#">Terms of Service</a> and <a className="font-semibold text-gray-900" href="">Privacy Police</a></p>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
--- my-project/src/pages/SignUp.jsx
+++ my-project/src/pages/SignUp.jsx
@@ -1,11 +1,12 @@
+import ImageHero from "../components/ImageHero"
 import SignUpForm from "../components/SignUpForm"
-
 
 function SignUp() {
     return(
-        <>
+        <div className="flex-col lg:flex flex-row">
+            <ImageHero />
             <SignUpForm />
-        </>
+        </div>
     )
 }
 
